49a227c13cad05c7969dec2e54dfa16342d2e512
Nirman Shrestha <nirmans39@gmail.com>
2026-04-16 06:56:00 +0000

    added : protected file for protected pages

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,14 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-gray-800 text-white p-4">
+        <h1 className="text-xl font-bold">Dashboard</h1>
+      </header>
+      <main className="flex-1 p-4">{children}</main>
+    </div>
+  );
+}
