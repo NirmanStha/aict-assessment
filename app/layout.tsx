412bca9fde68ae14b72b58023d15d0d465c56604
Nirman Shrestha <nirmans39@gmail.com>
2026-04-16 04:56:08 +0000

    add : login and register page, move providers to components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import TanstackQueryProvider from "@/providers/tanstack-query.provider";
+import TanstackQueryProvider from "@/components/providers/tanstack-query.provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
